a854b6e5475044c2440745cd2acdf208af3a736a
DimalshaR <148659027+DimalshaR@users.noreply.github.com>
2025-04-26 01:54:24 +0530

    Add files via upload

---
+++ backend/src/main/java/com/example/backend/repository/NotificationRepository.java
@@ -0,0 +1,12 @@
+package com.example.backend.repository;
+
+import com.example.backend.model.Notification;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import java.util.List;
+
+public interface NotificationRepository extends MongoRepository<Notification, String> {
+    List<Notification> findByReceiverIdOrderByCreatedAtDesc(String receiverId);
+    List<Notification> findByReceiverIdAndIsReadFalseOrderByCreatedAtDesc(String receiverId);
+    void deleteByReceiverId(String receiverId);
+
+}
