280602bd66138fd09784cde7a51e121f93771c56
DimalshaR <148659027+DimalshaR@users.noreply.github.com>
2025-05-16 15:20:44 +0530

    Update Notification.java
    updated the notification class

--- backend/src/main/java/com/example/backend/model/Notification.java
+++ backend/src/main/java/com/example/backend/model/Notification.java
@@ -4,6 +4,92 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.Date;
+import java.util.List;
+
+@Document(collection = "notifications")
+public class Notification {
+
+    @Id
+    private String id;
+
+    private String senderId;
+    private String receiverId;
+    private String message;
+    private boolean isRead;
+    private Date createdAt;
+    private String type;
+    private List<String> following; 
+
+    // --- Getters ---
+    public String getId() {
+        return id;
+    }
+
+    public String getSenderId() {
+        return senderId;
+    }
+
+    public String getReceiverId() {
+        return receiverId;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public boolean isRead() {
+        return isRead;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+    public String getType() {
+         return type;
+    }
+    public List<String> getFollowing() {
+        return following;
+    }
+    // --- Setters ---
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setSenderId(String senderId) {
+        this.senderId = senderId;
+    }
+
+    public void setReceiverId(String receiverId) {
+        this.receiverId = receiverId;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public void setIsRead(boolean isRead) {
+        this.isRead = isRead;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+    public void setType(String type) { 
+        this.type = type; 
+    }
+    public void setFollowing(List<String> following) {
+        this.following = following;
+    }
+}
+
+
+
+/*package com.example.backend.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Date;
 
 @Document(collection = "notifications")
 public class Notification {
@@ -67,3 +153,4 @@ public class Notification {
         this.createdAt = createdAt;
     }
 }
+*/
