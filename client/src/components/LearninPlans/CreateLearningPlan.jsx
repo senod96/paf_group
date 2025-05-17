f09fa80df1967e7a4b619f98d0d08a1e58e763a3
IT22089304 <135577528+IT22089304@users.noreply.github.com>
2025-05-17 17:32:03 +0530

    lahiru fix all error
    lahiru

--- client/src/components/LearninPlans/CreateLearningPlan.jsx
+++ client/src/components/LearninPlans/CreateLearningPlan.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
+import Navbar from '../Navbar';
 
 const CreateLearningPlan = () => {
   const userId = localStorage.getItem("user");
